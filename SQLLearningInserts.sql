164a9a0032147d011e57e169eab1621f562fa2db
Mike Baugus <mbaugus@gmail.com>
2018-01-31 14:09:54 -0500

    3rd day update

---
+++ SQLLearningInserts.sql
@@ -0,0 +1,13 @@
+select * from student
+
+insert into student values ('Mike', 'Baugus', 2.0, 1000, 1)
+insert into student values ('Nate', 'Dog', 2.0, 1100, 1)
+insert into student values ('Chris', 'Brown', 2.0, 1200, 3)
+insert into student values ('Steve', 'Guy', 2.0, 1300, 4)
+insert into student values ('Bob', 'Guy', 2.0, 1500, 2)
+insert into student values ('Christina', 'W', 2.0, 1250, 3)
+insert into student values ('Michael', 'Guy', 2.0, 1500, 3)
+insert into student values ('Jennifer', 'Person', 2.0, 1120, 2)
+insert into student values ('Cong', 'Guy', 2.0, 1010, 2)
+
+select * from student
